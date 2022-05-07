022079270299332c0ff7c144f34dd596b9330a5b
AcheronandStyx <matthew.danielson@trojans.dsu.edu>
2022-05-07 16:11:43 -0500

    indexedDB implemented, needs debugging

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,14 +1,35 @@
 let db; // create db connection variable
 
-const request = indexedDB.open("transactions", 1); // establish a connection to the db being created
+const request = indexedDB.open("offlineTransactions", 1); // establish a connection to the db being created
 
 // triggers on change to databases structure. Ex when it is first created or version changes
 // it then creates the object store to house transactions which were entered while offline
 request.onupgradeneeded = function (event) {
   const db = event.target.result;
-  db.createObjectStores("new_transaction", { autoIncrement: true }); // Set auto-increment to true to ensure unique indexes for each object and ease of data retrieval
+  db.createObjectStores("offline_transaction", { autoIncrement: true }); // Set auto-increment to true to ensure unique indexes for each object and ease of data retrieval
 };
 
-request.onsuccess = function(event) {
-    
+request.onsuccess = function (event) {
+  db = event.target.result;
+  // if app is online upload the new transaction
+  if (navigator.online) {
+    // uploadTransaction();
+  }
+};
+
+request.onerror = function (event) {
+  // show me the error if one occurs
+  console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+  console.log("entered saveRecord", record);
+  // open a new transaction with the db and grant read/write permission
+  const Transaction = db.transaction(["offline_transaction"], "readwrite");
+  console.log("transaction created", Transaction);
+  // access the objectStore
+  const transactionObjectStore = transaction.objectStore("new_transaction");
+
+  // add the offline transaction to the store
+  transactionObjectStore.add(record);
 }
