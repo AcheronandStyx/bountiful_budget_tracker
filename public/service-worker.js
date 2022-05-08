0060076ab72fac420f8d4c92ee9cf2c6b0b16ceb
AcheronandStyx <matthew.danielson@trojans.dsu.edu>
2022-05-08 18:47:37 -0500

    Deploying

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,7 +8,7 @@ const FILES_TO_CACHE = [
   "./js/idb.js",
   // "../models/transaction.js",
 ];
-
+/*
 // Respond with cached resources
 self.addEventListener("fetch", function (e) {
   console.log("fetch request : " + e.request.url);
@@ -26,7 +26,48 @@ self.addEventListener("fetch", function (e) {
     })
   );
 });
+*/
 
+self.addEventListener("fetch", function (event) {
+  // cache all get requests to /api routes
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(event.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(event.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return caches.match(event.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
 // Cache resources
 self.addEventListener("install", function (e) {
   e.waitUntil(
